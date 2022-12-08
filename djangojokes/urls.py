868db7d09f8a0fd39fc1e7932c1410b9986e5e67
chautrader <chautrader@gmail.com>
2022-12-08 14:01:18 +0100

    exercise 9 compleet

--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('jokes/', include('jokes.urls')),
     path('', include('pages.urls')),
 ]
 
